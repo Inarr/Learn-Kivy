3fb537c13148f195e2d7cc40e6bab74c34f44c85
Inarr <inarrd@gmail.com>
2025-03-14 09:42:04 -0400

    Add files via upload

--- The Lab/main.py
+++ The Lab/main.py
@@ -6,7 +6,15 @@ from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.stacklayout import StackLayout
 from kivy.metrics import dp
+from kivy.properties import StringProperty
 
+
+class WidgetExample(GridLayout):
+    my_text = StringProperty('Hello!')
+    def on_button_click(self):
+        print('Button Clicked')
+        self.my_text = 'You Clicked!'
+        
 
 class StackLayoutExample(StackLayout):
     def __init__(self, **kwargs):
